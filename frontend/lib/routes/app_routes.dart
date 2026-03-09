3a5fab1b701a5e1508f83c6cf1bf83620aaf261c
Thanooj Reddy <thanoojginne@gmail.com>
2026-03-10 00:55:47 +0530

    feat: Connect frontend modules with eachother.

---
+++ frontend/lib/routes/app_routes.dart
@@ -0,0 +1,16 @@
+class AppRoutes {
+  static const login = '/login';
+
+  static const studentDashboard = '/studentDashboard';
+  static const applyActivity = '/applyActivity';
+  static const studentPending = '/studentPending';
+
+  static const faDashboard = '/faDashboard';
+  static const faPending = '/faPending';
+  static const reviewApplication = '/reviewApplication';
+
+  static const adminDashboard = '/adminDashboard';
+  static const manageUsers = '/manageUsers';
+  static const manageCategories = '/manageCategories';
+  static const reports = '/reports';
+}
